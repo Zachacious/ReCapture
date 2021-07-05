85d8d3edf8b9ce9f68294ce8028119e333a6c0d0
zachacious <back4moore@gmail.com>
2021-07-05 12:32:42 -0500

    experiment

--- src-capacitor/android/app/src/main/java/zm/recapture/app/MainActivity.java
+++ src-capacitor/android/app/src/main/java/zm/recapture/app/MainActivity.java
@@ -1,5 +1,22 @@
 package zm.recapture.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.Plugin;
+
+//import java.util.ArrayList;
+
+import zm.recapture.app.SSDPPlugin;
+
+public class MainActivity extends BridgeActivity {
+@Override
+  public void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  registerPlugin(SSDPPlugin.class);
+//  this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+//    add(SSDPPlugin.class);
+//  }});
+}
+}
