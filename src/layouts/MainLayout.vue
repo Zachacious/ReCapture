37e2e1258775981ff55e4c8650b36cd16986df7d
zachacious <back4moore@gmail.com>
2021-07-11 16:32:27 -0500

    added logo, changed color theme, defaults to dark mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,23 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
+    <q-header>
+      <q-toolbar class="header-bar">
+        <!-- <q-btn
           flat
           dense
           round
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
-        />
+        /> -->
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <img src="logo.png" class="logo" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
+        <q-item-label header class="text-grey-8">
           Essential Links
         </q-item-label>
 
@@ -39,7 +27,7 @@
           v-bind="link"
         />
       </q-list>
-    </q-drawer>
+    </q-drawer> -->
 
     <q-page-container>
       <router-view />
@@ -48,72 +36,83 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+// import EssentialLink from "components/EssentialLink.vue";
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+// const linksList = [
+//   {
+//     title: "Docs",
+//     caption: "quasar.dev",
+//     icon: "school",
+//     link: "https://quasar.dev",
+//   },
+//   {
+//     title: "Github",
+//     caption: "github.com/quasarframework",
+//     icon: "code",
+//     link: "https://github.com/quasarframework",
+//   },
+//   {
+//     title: "Discord Chat Channel",
+//     caption: "chat.quasar.dev",
+//     icon: "chat",
+//     link: "https://chat.quasar.dev",
+//   },
+//   {
+//     title: "Forum",
+//     caption: "forum.quasar.dev",
+//     icon: "record_voice_over",
+//     link: "https://forum.quasar.dev",
+//   },
+//   {
+//     title: "Twitter",
+//     caption: "@quasarframework",
+//     icon: "rss_feed",
+//     link: "https://twitter.quasar.dev",
+//   },
+//   {
+//     title: "Facebook",
+//     caption: "@QuasarFramework",
+//     icon: "public",
+//     link: "https://facebook.quasar.dev",
+//   },
+//   {
+//     title: "Quasar Awesome",
+//     caption: "Community Quasar projects",
+//     icon: "favorite",
+//     link: "https://awesome.quasar.dev",
+//   },
+// ];
 
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-    EssentialLink
-  },
+  // components: {
+  //   EssentialLink,
+  // },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    // const leftDrawerOpen = ref(false);
 
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      // essentialLinks: linksList,
+      // leftDrawerOpen,
+      // toggleLeftDrawer() {
+      //   leftDrawerOpen.value = !leftDrawerOpen.value;
+      // },
+    };
+  },
+});
 </script>
+
+<style lang="scss" scoped>
+.header-bar {
+  background-color: $dark-page;
+  color: $dark-page-text;
+}
+
+.logo {
+  width: 120px;
+}
+</style>
