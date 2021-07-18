c205c0dc150098304bb95a60604dee7a1baaf9a1
zachacious <back4moore@gmail.com>
2021-07-18 13:02:02 -0500

    added bottom bar for modes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,32 +2,13 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar class="header-bar">
-        <!-- <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        /> -->
-
         <img src="logo.png" class="logo" />
+        <q-space />
+        <span class="flex flex-center text-bold ellipsis"
+          >Camera: {{ cameraName }}</span
+        >
       </q-toolbar>
     </q-header>
-
-    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
-      <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer> -->
 
     <q-page-container>
       <router-view />
@@ -37,53 +18,6 @@
 </template>
 
 <script>
-// import EssentialLink from "components/EssentialLink.vue";
-
-// const linksList = [
-//   {
-//     title: "Docs",
-//     caption: "quasar.dev",
-//     icon: "school",
-//     link: "https://quasar.dev",
-//   },
-//   {
-//     title: "Github",
-//     caption: "github.com/quasarframework",
-//     icon: "code",
-//     link: "https://github.com/quasarframework",
-//   },
-//   {
-//     title: "Discord Chat Channel",
-//     caption: "chat.quasar.dev",
-//     icon: "chat",
-//     link: "https://chat.quasar.dev",
-//   },
-//   {
-//     title: "Forum",
-//     caption: "forum.quasar.dev",
-//     icon: "record_voice_over",
-//     link: "https://forum.quasar.dev",
-//   },
-//   {
-//     title: "Twitter",
-//     caption: "@quasarframework",
-//     icon: "rss_feed",
-//     link: "https://twitter.quasar.dev",
-//   },
-//   {
-//     title: "Facebook",
-//     caption: "@QuasarFramework",
-//     icon: "public",
-//     link: "https://facebook.quasar.dev",
-//   },
-//   {
-//     title: "Quasar Awesome",
-//     caption: "Community Quasar projects",
-//     icon: "favorite",
-//     link: "https://awesome.quasar.dev",
-//   },
-// ];
-
 import { defineComponent, ref } from "vue";
 import alert from "../components/alert.vue";
 
@@ -93,21 +27,20 @@ export default defineComponent({
     alert,
   },
 
-  // components: {
-  //   EssentialLink,
-  // },
-
   setup() {
-    // const leftDrawerOpen = ref(false);
+    return {};
+  },
 
+  data() {
     return {
-      // essentialLinks: linksList,
-      // leftDrawerOpen,
-      // toggleLeftDrawer() {
-      //   leftDrawerOpen.value = !leftDrawerOpen.value;
-      // },
+      tab: "tab1",
+      cameraName: this.$connection.data.device.friendlyName,
     };
   },
+
+  // mounted() {
+  //   console.log(this.$connection);
+  // },
 });
 </script>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,48 @@
 <template>
-  <q-page class="flex flex-center"> Home Screen </q-page>
+  <q-page class="flex flex-center">
+    <q-card flat class="tab-area full-width flex flex-center bg-repurple">
+      <q-tab-panels
+        v-model="tab"
+        animated
+        class="tab-panels full-width bg-repurple"
+      >
+        <q-tab-panel name="capture" class="tab-panel-area">
+          <div class="text-h6">Mails</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="playback">
+          <div class="text-h6">Alarms</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+
+        <q-tab-panel name="info">
+          <div class="text-h6">Movies</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+
+      <div class="tab-button-holder full-width">
+        <q-tabs
+          v-model="tab"
+          shrink
+          indicator-color="transparent"
+          active-color="reblue"
+          class="tabbar text-relav bg-repurple"
+        >
+          <q-tab name="capture">
+            <q-icon name="mdi-camera-iris" size="md" />
+          </q-tab>
+          <q-tab name="playback">
+            <q-icon name="mdi-play-circle" size="md" />
+          </q-tab>
+          <q-tab name="info">
+            <q-icon name="mdi-information" size="md" />
+          </q-tab>
+        </q-tabs>
+      </div>
+    </q-card>
+  </q-page>
 </template>
 
 <script>
@@ -9,13 +52,43 @@ export default defineComponent({
   name: "PageIndex",
   data() {
     return {
+      tab: "capture",
       value: "",
     };
   },
-  methods: {},
+  methods: {
+    async adjustFullScreenTabs() {
+      const header = document.querySelector(".header-bar");
+      const tabbar = document.querySelector(".tabbar");
+      const tabpanel = document.querySelector(".tab-panels");
+      if (header && tabbar && tabpanel) {
+        console.log("here");
+        tabpanel.style.height = `calc(100% - ${header.offsetHeight}px)`;
+      }
+    },
+  },
+
+  async beforeUpdate() {
+    this.adjustFullScreenTabs();
+  },
 
   async mounted() {
     this.$q.capacitor.Plugins.SplashScreen.hide();
+    this.adjustFullScreenTabs();
   },
 });
 </script>
+
+<style scoped lang="scss">
+.tab-area {
+  align-self: stretch;
+}
+
+.tab-panels {
+  align-self: stretch;
+}
+
+.tab-button-holder {
+  border-top: 1px solid $old-lavendar;
+}
+</style>
